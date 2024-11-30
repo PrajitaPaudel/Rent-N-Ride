8a10cc7038c398ea828226e502cfdfdcc854c736
PrajitaPaudel <101269080+PrajitaPaudel@users.noreply.github.com>
2024-11-30 11:13:59 +0545

    Update user_type_controller.dart

--- lib/controller/user_controller/user_type_controller.dart
+++ lib/controller/user_controller/user_type_controller.dart
@@ -1,10 +1,10 @@
 import 'package:get/get.dart';
 
 class UserTypeController extends GetxController {
-  // User type selection
+
   var selectedUserType = ''.obs;
 
-  // Method to update the selected user type
+  
   void setUserType(String userType) {
     selectedUserType.value = userType;
   }
@@ -12,10 +12,10 @@ class UserTypeController extends GetxController {
 
 
 class AvailableController extends GetxController {
-  // Available type selection, now using bool
+  
   var selectedAvailableType = false.obs;
 
-  // Method to update the selected available type
+  
   void setAvailableType(bool available) {
     selectedAvailableType.value = available;
   }
@@ -25,10 +25,10 @@ class AvailableController extends GetxController {
 
 
 class PPopularController extends GetxController {
-  // Popular type selection, now using bool
+
   var selectedPopularType = false.obs;
 
-  // Method to update the selected popular type
+ 
   void setPopularType(bool popular) {
     selectedPopularType.value = popular;
   }
@@ -36,10 +36,10 @@ class PPopularController extends GetxController {
 
 
 class InsuranceController extends GetxController {
-  // Available type selection, now using bool
+
   var selectedInsuranceType = false.obs;
 
-  // Method to update the selected available type
+
   void setInsuranceType(bool yes) {
     selectedInsuranceType.value = yes;
   }
